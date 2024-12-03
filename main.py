03cee4ee2008582821cf1cdba5ece302e58fa6b9
a k <ak97plutonium@gmail.com>
2024-12-03 17:32:52 +0530

    created main.py
    main.py created with card list and while loop to run the game.

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+# these are all the cards in the game.
+CARDS = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10
+]
+
+# var to run game.
+game_onn : bool = True
+
+
+# loop to run the game.
+while game_onn:
+
+    break
