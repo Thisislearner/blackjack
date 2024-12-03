29c69967629e37872b60a79967b6e795215c43d2
a k <ak97plutonium@gmail.com>
2024-12-03 17:42:11 +0530

    draw_card()
    this function draws a random card from draw_card function

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import random
 
+
+def draw_card() -> int :
+    """draw card from deck"""
+    cards : list = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10
+    ]
+    return random.choice(cards)
+
+
 # these are all the cards in the game.
-CARDS = [
+PARENT_CARDS_DECK = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10
 ]
 
 # var to run game.
 game_onn : bool = True
 
-
 # loop to run the game.
 while game_onn:
 
+    # delete this break after the program is done.
     break
