b010d13ca4540b1e0c901013aae669c0a877ca5e
a k <ak97plutonium@gmail.com>
2024-12-03 17:54:25 +0530

    deck_sum
    funciton to calculate total score

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ def draw_card() -> int :
     return random.choice(cards)
 
 
+def deck_sum(input_deck : list) -> int:
+    """calculates total the deck"""
+    total = 0  # var to store sum total of deck.
+    for i in input_deck:
+        total += i # adding ith item.
+    return total
+
+
 # these are all the cards in the game.
 PARENT_CARDS_DECK = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10
